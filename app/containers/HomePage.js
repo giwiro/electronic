07fe1994c368f0b9bb1c4eae0329c1c015ca175b
Gi Wah Dávalos Loo <giwirodavalos@gmail.com>
2017-07-23 21:28:49 -0500

    initial commit

---
+++ app/containers/HomePage.js
@@ -0,0 +1,14 @@
+// @flow
+import React, { Component } from 'react';
+import Home from '../components/Home';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>holis</h1>
+        <Home />
+      </div>
+    );
+  }
+}
